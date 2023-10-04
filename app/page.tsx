201cb608077a3e5dce772e3a6ec47ccc4bd390c2
Prince Trivedi <122072887+3vdi@users.noreply.github.com>
2023-10-04 10:45:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,50 +5,194 @@ import Marquee from "react-fast-marquee";
 export default function Home() {
   return (
     <div>
-      <Marquee>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/7Z7WVzJsSReG8B0CaPk0bvWD7tK.jpg"
-            width="400px"
-            height="225px"
-          />
-        </div>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/5FROLD8zpWFs9ja7aYho1uOMJHg.jpg"
-            width="150px"
-            height="225px"
-          />
-        </div>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/pngbqfh7YZUgJs4Fn5368Sw29E2.jpg"
-            width="150px"
-            height="225px"
-          />
-        </div>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/tDFvXn4tane9lUvFAFAUkMylwSr.jpg"
-            width="400px"
-            height="225px"
-          />
-        </div>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/gJCRERaZs0NvHktBsfDllc09pE1.jpg"
-            width="150px"
-            height="225px"
-          />
-        </div>
-        <div class="m-2">
-          <img
-            src="https://www.themoviedb.org/t/p/original/1vZAjTt2RW5mhOsO6K0KnLve612.jpg"
-            width="150px"
-            height="225px"
-          />
-        </div>
-      </Marquee>
+      <div>
+        <Marquee speed="20">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/7Z7WVzJsSReG8B0CaPk0bvWD7tK.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/5FROLD8zpWFs9ja7aYho1uOMJHg.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/pngbqfh7YZUgJs4Fn5368Sw29E2.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/cuiJ6e6kSatyRlnCJrvJG2ElpSg.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/gJCRERaZs0NvHktBsfDllc09pE1.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/1vZAjTt2RW5mhOsO6K0KnLve612.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="20" direction="right">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/kmUva5IBqb2ca2uWju2GAiPK2ed.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/2t0B3SXsh27O7BFtc9eaEpHVE6Y.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/w70adOkHiMb7tEWOmFQY5OZjsl.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/tDFvXn4tane9lUvFAFAUkMylwSr.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/1vZAjTt2RW5mhOsO6K0KnLve612.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="20">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/4GEvRItXRRRdrmx12vufTC4Pubk.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/bk3IGSc9KtSeJzypNWZySZbiU9m.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/x4X1zfPWp3W5Q3xIJ8ljxPqMJGF.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/eCeOqa1suTS9njg76ai9xJr1biy.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/8TEs6twIANAc2qiU8m17JO8Ujcx.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/y0VnJt4eRPMjA1hpJ8f1EFoVaSf.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="20" direction="right">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/mvpPslST6buOd5UmVE8lPkfqTk3.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/6pZgH10jhpToPcf0uvyTCPFhWpI.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/tOSnFE9e82iH3ZAzSTtuOkBsabJ.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/shqLeIkqPAAXM8iT6wVDiXUYz1p.jpg"
+              width="400px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/xYponWgvJcpYATPEx5qjzcigVOe.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/original/cMYCDADoLKLbB83g4WnJegaZimC.jpg"
+              width="150px"
+              height="225px"
+            />
+          </div>
+        </Marquee>
+      </div>
     </div>
   );
 }
+
