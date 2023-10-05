adab7391aba4a11a87f74ca5b5610ff19182e52d
Prince Trivedi <122072887+3vdi@users.noreply.github.com>
2023-10-05 14:21:43 +0530

    Create Homepage.tsx

---
+++ app/Homepage.tsx
@@ -0,0 +1,224 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+export default function Homepage() {
+  return (
+    <div>
+      <div>
+        <Marquee speed="14">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/7Z7WVzJsSReG8B0CaPk0bvWD7tK.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/5FROLD8zpWFs9ja7aYho1uOMJHg.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/pngbqfh7YZUgJs4Fn5368Sw29E2.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/cuiJ6e6kSatyRlnCJrvJG2ElpSg.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/gJCRERaZs0NvHktBsfDllc09pE1.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/zat2MMhejQyJJN6CucLI9Or9kdo.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/imW5JQccrhJtihQlgOckEc4ZdEe.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="10" direction="right">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/kmUva5IBqb2ca2uWju2GAiPK2ed.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/2t0B3SXsh27O7BFtc9eaEpHVE6Y.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/w70adOkHiMb7tEWOmFQY5OZjsl.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/tDFvXn4tane9lUvFAFAUkMylwSr.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/1vZAjTt2RW5mhOsO6K0KnLve612.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/2x7D94yCfGCcTn83SGfZjVejsrF.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="14">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/4GEvRItXRRRdrmx12vufTC4Pubk.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/bk3IGSc9KtSeJzypNWZySZbiU9m.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/x4X1zfPWp3W5Q3xIJ8ljxPqMJGF.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/eCeOqa1suTS9njg76ai9xJr1biy.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/8TEs6twIANAc2qiU8m17JO8Ujcx.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/y0VnJt4eRPMjA1hpJ8f1EFoVaSf.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/kMntkrTv9DrCvm9HXpIVCEGp7J4.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+        </Marquee>
+      </div>
+
+      <div>
+        <Marquee speed="10" direction="right">
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/mvpPslST6buOd5UmVE8lPkfqTk3.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/6pZgH10jhpToPcf0uvyTCPFhWpI.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/tOSnFE9e82iH3ZAzSTtuOkBsabJ.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w500_and_h282_face/shqLeIkqPAAXM8iT6wVDiXUYz1p.jpg"
+              width="320px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/xYponWgvJcpYATPEx5qjzcigVOe.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/cMYCDADoLKLbB83g4WnJegaZimC.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+          <div class="m-2">
+            <img
+              src="https://www.themoviedb.org/t/p/w185_and_h278_multi_faces/v6qgdAjdVepVda2XHMgCKnwpiom.jpg"
+              width="120px"
+              height="144px"
+            />
+          </div>
+        </Marquee>
+      </div>
+    </div>
+  );
+}
